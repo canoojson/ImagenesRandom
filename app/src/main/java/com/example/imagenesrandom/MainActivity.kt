2a819e68d97c143c4b11d4e182afd52cba9d3c1a
Sergio <Sergio.Cano.Pomer.2004@gmail.com>
2024-09-25 21:06:35 +0200

    Aplicación terminada

--- app/src/main/java/com/example/imagenesrandom/MainActivity.kt
+++ app/src/main/java/com/example/imagenesrandom/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.example.imagenesrandom
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
@@ -23,7 +20,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -75,12 +71,11 @@ fun ImagenesRandom(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-            Image(
-                painter = painterResource(id = selectedImageResId),
-                contentDescription = null,
-                modifier = Modifier.size(200.dp)
-            )
-            Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            painter = painterResource(id = selectedImageResId),
+            contentDescription = null,
+            modifier = Modifier.size(200.dp)
+        )
         Button(onClick = { showRandomImage() }) {
             Text(text = "Mostrar imagen aleatoria")
         }
