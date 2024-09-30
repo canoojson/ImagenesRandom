885204b941bae6d84a6e56d1477f2216d617dbfe
Sergio Cano <Sergio.Cano.Pomer.2004@gmail.com>
2024-09-30 08:22:30 +0200

    Arreglado 😎🤜

--- app/src/main/java/com/example/imagenesrandom/MainActivity.kt
+++ app/src/main/java/com/example/imagenesrandom/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.imagenesrandom
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -18,12 +17,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -55,33 +53,30 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun ImagenesRandom(modifier: Modifier = Modifier) {
 
-    val imageResIds = listOf(
-        R.drawable.rocktransgresivo,
-        R.drawable.deltoya,
-        R.drawable.materialdefectuoso,
-        R.drawable.yominoriaabsoluta,
-        R.drawable.cancionesprohibidas
-    )
+    var result by remember { mutableIntStateOf(0) }
 
-    var selectedImageResId by remember { mutableStateOf(R.drawable.rocktransgresivo) }
-
-    fun showRandomImage() {
-        val randomIndex = Random.nextInt(0, 5)
-        selectedImageResId = imageResIds[randomIndex]
+    val image = when (result){
+        1 -> R.drawable.rocktransgresivo
+        2 -> R.drawable.deltoya
+        3 -> R.drawable.materialdefectuoso
+        4 -> R.drawable.yominoriaabsoluta
+        5 -> R.drawable.cancionesprohibidas
+        else -> R.drawable.cancionesprohibidas
     }
 
-    Column(
+
+Column(
         modifier = Modifier.fillMaxSize().padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
             Image(
-                painter = painterResource(id = selectedImageResId),
+                painter = painterResource(id = image),
                 contentDescription = null,
                 modifier = Modifier.size(200.dp)
             )
             Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { showRandomImage() }) {
+        Button(onClick = { result = Random.nextInt(1,6) }) {
             Text(text = "Mostrar imagen aleatoria")
         }
         }
